a5c71c38645b9eeb4aadfc1fc850132be0ed1dbe
Shama Ahlawat <shama@oriserve.com>
2018-12-12 13:06:15 +0530

    initial setup

---
+++ Shama/Shama/Shama.h
@@ -0,0 +1,19 @@
+//
+//  Shama.h
+//  Shama
+//
+//  Created by Shama Ahlawat on 12/12/18.
+//  Copyright © 2018 oriserve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shama.
+FOUNDATION_EXPORT double ShamaVersionNumber;
+
+//! Project version string for Shama.
+FOUNDATION_EXPORT const unsigned char ShamaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shama/PublicHeader.h>
+
+
